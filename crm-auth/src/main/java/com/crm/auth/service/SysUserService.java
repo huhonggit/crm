40a89237ace4f3708e7a6d76b53d5a4c02b9ff9f
huhong <huhong@cheesestar.com>
2019-06-22 14:50:18 +0800

    mongodb 连接池

--- crm-auth/src/main/java/com/crm/auth/service/SysUserService.java
+++ crm-auth/src/main/java/com/crm/auth/service/SysUserService.java
@@ -1,15 +1,6 @@
 package com.crm.auth.service;
 
-import com.alibaba.fastjson.JSON;
-import com.crm.auth.po.SysUser;
-import com.crm.common.enums.StatusType;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.math.BigDecimal;
 
 /**
  * 系统用户服务类
@@ -17,34 +8,7 @@ import java.math.BigDecimal;
  * @date 2019-06-21 19:23
  */
 @Service
-public class SysUserService implements InitializingBean {
+public class SysUserService {
 
 
-    @Resource
-    private RedisTemplate redisTemplate;
-
-    @Resource
-    private MongoTemplate mongoTemplate;
-
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        SysUser user = new SysUser();
-        user.setName("sd");
-        user.setStatus(StatusType.enable);
-
-        redisTemplate.opsForValue().set("a",user );
-
-
-        SysUser a = (SysUser) redisTemplate.opsForValue().get("a");
-        System.out.println("测试");
-        System.out.println(JSON.toJSONString(a));
-
-        Test test = new Test();
-        test.setTt(new BigDecimal("22.345"));
-
-        mongoTemplate.insert(test);
-        System.out.println("2323131");
-
-    }
 }
